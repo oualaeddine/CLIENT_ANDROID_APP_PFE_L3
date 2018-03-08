06975476aef8304601e6fb13a10e80b56e384460
berrehal ouala eddine <berrehaloualaeddine@gmail.com>
2018-03-08 14:05:59 +0100

    first commit

---
+++ app/src/main/java/berrehal_rahab_benghezal/pfe/l3app_client/MainActivity.java
@@ -0,0 +1,13 @@
+package berrehal_rahab_benghezal.pfe.l3app_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
